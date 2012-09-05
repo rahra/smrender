57c6c3294d93a6ebe36d0df8b22b548ab79964bb
lbfiser <lbfiser@98589cd8-2cc7-e511-97f5-005056b96d30>
2012-09-05 10:54:08 +0000

    export of KAP header files implemented (option -k)

--- trunk/src/bxtree.c
+++ trunk/src/bxtree.c
@@ -1,3 +1,20 @@
+/* Copyright 2011 Bernhard R. Fischer, 2048R/5C5FFD47 <bf@abenteuerland.at>
+ *
+ * This file is part of smfilter.
+ *
+ * Smfilter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Smfilter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with smfilter. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
