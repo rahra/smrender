2e6017aec311ea115f73e769541e52cdfd6aee72
lbfiser <lbfiser@98589cd8-2cc7-e511-97f5-005056b96d30>
2012-11-12 08:14:11 +0000

    reader-writer-lock added to bxtree functions get() and add().

--- trunk/src/bxtree.c
+++ trunk/src/bxtree.c
@@ -15,6 +15,9 @@
  * along with smfilter. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -23,6 +26,10 @@
 #include "smrender.h"
 #include "bxtree.h"
 
+#ifdef WITH_THREADS
+#include <pthread.h>
+static pthread_rwlock_t rwlock_ = PTHREAD_RWLOCK_INITIALIZER;
+#endif
 
 static size_t mem_usage_ = 0;
 static long malloc_cnt_ = 0;
@@ -86,7 +93,7 @@ void bx_free(bx_node_t *node)
  * @param d curren depth of tree.
  * @return pointer to added node.
  */
-bx_node_t *bx_add_node0(bx_node_t **node, bx_hash_t h, bx_hash_t d)
+bx_node_t *bx_add_node1(bx_node_t **node, bx_hash_t h, bx_hash_t d)
 {
    // create new empty node if 'this' doesn't exist
    if (*node == NULL)
@@ -97,18 +104,48 @@ bx_node_t *bx_add_node0(bx_node_t **node, bx_hash_t h, bx_hash_t d)
       return *node;
 
    // otherwise recurse
-   return bx_add_node0((bx_node_t**) &((*node)->next[BT_MASK(h, d)]), h, d + 1);
+   return bx_add_node1((bx_node_t**) &((*node)->next[BT_MASK(h, d)]), h, d + 1);
 }
 
 
-bx_node_t *bx_get_node0(bx_node_t *node, bx_hash_t h, bx_hash_t d)
+bx_node_t *bx_add_node0(bx_node_t **node, bx_hash_t h, bx_hash_t d)
+{
+#ifdef WITH_THREADS
+   bx_node_t *nd;
+
+   pthread_rwlock_wrlock(&rwlock_);
+   nd = bx_add_node1(node, h, d);
+   pthread_rwlock_unlock(&rwlock_);
+   return nd;
+#else
+   return bx_add_node1(node, h, d);
+#endif
+}
+
+
+bx_node_t *bx_get_node1(bx_node_t *node, bx_hash_t h, bx_hash_t d)
 {
    // break recursion at end of depth
    if (d >= ((sizeof(bx_hash_t) * 8) / BX_RES))
       return node;
 
    // test for null pointer and recurse if applicable
-   return node ? bx_get_node0(node->next[BT_MASK(h, d)], h, d + 1) : node;
+   return node ? bx_get_node1(node->next[BT_MASK(h, d)], h, d + 1) : node;
+}
+
+
+bx_node_t *bx_get_node0(bx_node_t *node, bx_hash_t h, bx_hash_t d)
+{
+#ifdef WITH_THREADS
+   bx_node_t *nd;
+
+   pthread_rwlock_rdlock(&rwlock_);
+   nd = bx_get_node1(node, h, d);
+   pthread_rwlock_unlock(&rwlock_);
+   return nd;
+#else
+   return bx_get_node1(node, h, d);
+#endif
 }
 
 
