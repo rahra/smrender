ecd5c3bc99574d6b261c776bbd289a0fa7020c1e
eagle <eagle@98589cd8-2cc7-e511-97f5-005056b96d30>
2015-11-29 13:01:56 +0000

    Control point calculation for bspline updated: fixed corner case if two subsequent lines have exactly the same angle.

--- trunk/src/smath.h
+++ trunk/src/smath.h
@@ -20,6 +20,7 @@ void coord_diffp(const struct coord *, const struct coord *, struct pcoord *);
 struct pcoord coord_diff(const struct coord *, const struct coord *);
 struct coord dest_coord(const struct coord *, const struct pcoord *);
 double fmod2(double , double );
+int sgn(double );
 
 
 #endif
