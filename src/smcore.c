d0d16d5e406ed6e6107ba43c18a0937d525d87f5
Bernhard R. Fischer <bf@abenteuerland.at>
2026-03-16 18:29:22 +0100

    rule tag code slightly rewritten, should finally work now

--- src/smcore.c
+++ src/smcore.c
@@ -39,6 +39,7 @@
 #include <sys/time.h>   // gettimeofday
 
 #include "smrender.h"
+#include "smrender_dev.h"
 #include "smcore.h"
 #include "smaction.h"
 #include "rdata.h"
@@ -66,147 +67,33 @@ void __attribute__((destructor)) print_trv_time(void)
 
 //#define ADD_RULE_TAG
 #ifdef ADD_RULE_TAG
-/*FIXME: The following code does not work realiably yet because at some place
- * tags are copied from one object to another. Those are shallow copies, thus
- * the string pointers of the rules tag point to the same area and are thus
- * overwritten from one object to another.
- */
-
 //! fixed length of rule-tag to avoid realloc()
 #define RULES_TAG_LEN 4096
 
-/*! Compare string s2 to the initial part of bstring_t s1, i.e. strlen(s2) number of
- * characters.
- */
-static int bs_ncmp2(const bstring_t *s1, const char *s2)
-{
-   for (int i = 0; i < s1->len && *s2; i++, s2++)
-      if (s1->buf[i] != *s2)
-         return s1->buf[i] - *s2;
-   if (!*s2)
-      return 0;
-   return -*s2;
-}
 
-
-/*! This function checks if the rules tag in b matches the OSM object o. It
- * checks the format of "'node' | 'way' | 'relation' '=' <id>" where the type
- * (node/way/relation) and the id must match that of object o.
- * @param o Pointer to OSM object.
- * @param b bstring of rules tag.
- * @return If the rule tag matches the object, 0 is return. In all other cases
- * -1 is returned.
- */
-static int check_rule_tag(const osm_obj_t *o, bstring_t b)
-{
-   long id;
-   char c;
-   int i;
-
-   // check if object type (node/way/relation) matches rules tag
-   if (bs_ncmp2(&b, type_str(o->type)))
-   {
-      log_debug("rule type mismatch of %s %"PRId64", tag = '%.*s'", type_str(o->type), o->id, b.len, b.buf);
-      return -1;
-   }
-   i = strlen(type_str(o->type));
-   if (b.len <= i)
-   {
-      log_debug("rule tag truncated of '%s' %"PRId64, type_str(o->type), o->id);
-      return -1;
-   }
-   bs_nadvance(&b, i);
-   // check for separator '='
-   if (*b.buf != '=')
-   {
-      log_debug("syntax error in rule tag truncated of '%s' %"PRId64, type_str(o->type), o->id);
-      return -1;
-   }
-   bs_advance(&b);
-   // return if there is nothing behind '='
-   if (!b.len)
-   {
-      log_debug("syntax error in rule tag truncated of '%s' %"PRId64, type_str(o->type), o->id);
-      return -1;
-   }
-   if (*b.buf == '-')
-   {
-      if (b.len <= 1)
-      {
-         log_debug("syntax error in rule tag truncated of '%s' %"PRId64, type_str(o->type), o->id);
-         return -1;
-      }
-      c = b.buf[1];
-   }
-   else
-      c = *b.buf;
-   if (c < '0' || c > '9')
-   {
-      log_debug("syntax error in rule tag truncated of '%s' %"PRId64, type_str(o->type), o->id);
-      return -1;
-   }
-   id = bs_tol(b);
-   if (id != o->id)
-   {
-      log_debug("id mismatch in rule tag of %"PRId64" (is %"PRId64")", o->id, id);
-      return -1;
-   }
-   return 0;
-}
-
-
-/*! Allocate memory for rules tag string b and initialize it.
- * The function allocates a fixed size heap area of RULES_TAG_LEN. It must
- * not be reallocated (with realloc())!
- * @param o Pointer to object for which the rule tag should be initialized.
- * @param b Pointer to bstring which receives the rule tag.
- * @return On success, 0 is returned, otherwise -1.
- */
-int alloc_init_rule_tag(const osm_obj_t *o, bstring_t *b)
-{
-   char *s;
-   if ((s = malloc(RULES_TAG_LEN)) == NULL)
-   {
-      log_errno(LOG_ERR, "malloc() failed");
-      return -1;
-   }
-   b->buf = s;
-   if ((b->len = snprintf(b->buf, RULES_TAG_LEN, "%s=%"PRId64, type_str(o->type), o->id)) < 0)
-   {
-      log_msg(LOG_ERR, "snprintf() failed. WTF?");
-      b->len = 0;
-   }
-   // safety check
-   if (b->len > RULES_TAG_LEN)
-      b->len = RULES_TAG_LEN;
-
-   return 0;
-}
-
- 
 static int add_rule_tag(const smrule_t *r, osm_obj_t *o)
 {
    char buf[64];
    int n, plen;
    bstring_t b;
 
+   // make rules tag key
+   if ((plen = snprintf(buf, sizeof(buf), RULES_TAG":%"PRId64, o->id)) < 0)
+   {
+      log_msg(LOG_ERR, "snprintf() failed");
+      return -1;
+   }
+   if (plen >= (int) sizeof(buf))
+   {
+      log_msg(LOG_ERR, "output buffer for snprintf() too small");
+      return -1;
+   }
+
    // check if object already has a rules tag
-   if ((n = match_attr(o, RULES_TAG, NULL)) >= 0)
+   if ((n = match_attr(o, buf, NULL)) >= 0)
    {
       // rules tag exists, check it
       b = o->otag[n].v;
-      if (!b.len)
-      {
-         log_msg(LOG_EMERG, "Rules-tag without content! This may indicate a bug.");
-         return -1;
-      }
-      // check format of rules tag and allocate a new one in case of error
-      if (check_rule_tag(o, b))
-      {
-         log_debug("reallocating incorrect rule tag '%.*s' for %s %"PRId64, b.len, b.buf, type_str(o->type), o->id);
-         if (alloc_init_rule_tag(o, &b) == -1)
-            return -1;
-      }
    }
    else
    {
@@ -223,13 +110,18 @@ static int add_rule_tag(const smrule_t *r, osm_obj_t *o)
       b.len = o->otag[n].v.len = 0;
       b.buf = o->otag[n].v.buf = NULL;
 
-      o->otag[n].k.buf = RULES_TAG;
-      o->otag[n].k.len = strlen(o->otag[n].k.buf);
+      o->otag[n].k.buf = smstrdup(buf);
+      o->otag[n].k.len = plen;
 
-      if (alloc_init_rule_tag(o, &b) == -1)
+      if ((b.buf = malloc(RULES_TAG_LEN)) == NULL)
+      {
+         log_errno(LOG_ERR, "cannot get memory for rules tag");
          return -1;
+      }
+      b.len = 0;
    }
 
+   // make rules tag value
    if ((plen = snprintf(buf, sizeof(buf), "%"PRId64, r->oo->id)) < 0)
    {
       log_msg(LOG_ERR, "snprintf() failed");
@@ -249,8 +141,11 @@ static int add_rule_tag(const smrule_t *r, osm_obj_t *o)
       return -1;
    }
 
+   // prepend ';'
    if (b.len)
       b.buf[b.len++] = ';';
+
+   // append rule id string to rules tag
    memcpy(b.buf + b.len, buf, plen);
    b.len += plen;
 
